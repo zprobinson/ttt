6777099e8e153235e33eb075561242f9825e519f
Zach Robinson <Zachary.Robinson@interstate.com>
2024-03-20 14:25:33 -0500

    Add internal represenatation of game board.

--- src/dtos.ts
+++ src/dtos.ts
@@ -1,89 +1,101 @@
 const boardPositions = [0, 1, 2, 3, 4, 5, 6, 7, 8] as const;
-type BoardPosition = typeof boardPositions[number];
+type BoardPosition = (typeof boardPositions)[number];
 
 type ArchivedGame = {
-    winner: Winner | 'unknown'
-    turns: {
-        value: PositionValue,
-        position: FullPosition
-        lengthSeconds: number
-    }[]
-}
+  winner: Winner | "unknown";
+  turns: {
+    value: PositionValue;
+    position: FullPosition;
+    lengthSeconds: number;
+  }[];
+};
 
 export type ArchivedSeries = {
-    id: SeriesId
-    name: string
-    archivedAt: Date
-    description: string
-    timeControlSeconds: number
-    games: ArchivedGame[]
-}
+  id: SeriesId;
+  name: string;
+  archivedAt: Date;
+  description: string;
+  timeControlSeconds: number;
+  games: ArchivedGame[];
+};
 
 export type AvailableMoveResponse = {
-    turn: TurnInProgress
-    available: FullPosition[]
-}
+  turn: TurnInProgress;
+  available: FullPosition[];
+};
 
 export type CreateSeriesRequest = {
-    gameCount: number
-    timeControlSeconds: number
-    seriesName: string
-    description: string
-}
+  gameCount: number;
+  timeControlSeconds: number;
+  seriesName: string;
+  description: string;
+};
 
 export type FullPosition = `${BoardPosition}:${BoardPosition}`;
 
 export type GameSummary = {
-    turnsTaken: number
-    secondsUsed: {
-        x: number
-        o: number
-    }
-    currentTurn: [PositionValue] | []
-    availableMoves: FullPosition[]
-    previousMoves: {value: PositionValue, position: FullPosition}[]
-}
+  turnsTaken: number;
+  secondsUsed: {
+    x: number;
+    o: number;
+  };
+  currentTurn: [PositionValue] | [];
+  availableMoves: FullPosition[];
+  previousMoves: { value: PositionValue; position: FullPosition }[];
+};
+
+export type Cell = { position: BoardPosition; value: PositionValue | "empty" };
+export type TicTacToeBoard = [
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell]
+];
+export type SuperBoard = [
+  [TicTacToeBoard, TicTacToeBoard, TicTacToeBoard],
+  [TicTacToeBoard, TicTacToeBoard, TicTacToeBoard],
+  [TicTacToeBoard, TicTacToeBoard, TicTacToeBoard]
+];
 
 export type IdLookupResponse = {
-    id: PlayerId | SeriesId
-    seriesId: SeriesId
-    type: 'playerX' | 'playerO' | 'series'
-}
+  id: PlayerId | SeriesId;
+  seriesId: SeriesId;
+  type: "playerX" | "playerO" | "series";
+};
 
 export type PlayerId = string;
 
-export type PositionValue = 'X' | 'O';
+export type PositionValue = "X" | "O";
 
 export type SeriesCreated = {
-    id: SeriesId
-    playerIds: {
-        x: PlayerId
-        o: PlayerId
-    }
-    timeControlSeconds: number
-    totalGamesCount: number
-}
+  id: SeriesId;
+  playerIds: {
+    x: PlayerId;
+    o: PlayerId;
+  };
+  timeControlSeconds: number;
+  totalGamesCount: number;
+};
 
 export type SeriesId = string;
 
 export type SeriesSummary = {
-    id: SeriesId
-    name: string
-    description: string
-    currentGame: [GameSummary] | []
-    timeControlSeconds: number
-    gamesCompletedCount: number
-    totalGamesCount: number
-}
+  id: SeriesId;
+  name: string;
+  description: string;
+  currentGame: [GameSummary] | [];
+  timeControlSeconds: number;
+  gamesCompletedCount: number;
+  totalGamesCount: number;
+};
 
 export type TurnInProgress = {
-    startTime: Date
-    toMove: PositionValue
-}
+  startTime: Date;
+  toMove: PositionValue;
+};
 
 type WebHooksSubscription = {
-    playerId: PlayerId
-    url: string
-}
+  playerId: PlayerId;
+  url: string;
+};
 
-export type Winner = 'X' | 'O' | 'CAT'
+export type Winner = "X" | "O" | "CAT";
